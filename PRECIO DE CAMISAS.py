e0c8b31f818bb2aa3ba545614d1790a20a186692
priscigu <110804906+priscigu@users.noreply.github.com>
2022-08-23 00:45:37 -0400

    Add files via upload

---
+++ PRECIO DE CAMISAS.py
@@ -0,0 +1,17 @@
+numerodecamisas=int(input("Ingrese cuantas camisas compro"))
+precio=int(input("¿Cual es le precio por camisa?"))
+total=int(input("El total es"))
+descuento=int(input("Su descuento es de"))
+totalmasdescuento= int(input("El total que debe pagar mas el descuento es"))
+total=numerodecamisas*precio:
+if numerocamisas>3:
+    descuento = total*0.20       
+else:
+    descuento = total*0.10
+totalmasdescuento=total-descuento
+print("El total que debe pagar mas el descuento es",totalmasdescuento)
+
+    
+        
+
+
