ddf816bcb4c64a36fa94774f6a9fd8df7c9c7457
priscigu <110804906+priscigu@users.noreply.github.com>
2022-08-23 00:02:25 -0400

    Add files via upload

---
+++ NUMERO MAYOR.py
@@ -0,0 +1,17 @@
+numero1=int(input("Ingrese el primer numero"))
+numero2=int(input("Ingrese el segundo numero"))
+numero3=int(input("Ingrese el tercer numero"))
+if numero1>numero2:
+    print("El numero mayor es",numero1)
+if  numero1>numero3:
+    print("El numero mayor es",numero1)
+if numero2>numero3:
+    print("El numero mayor es",numero2)
+if numero2>numero3:
+    print("El numero mayor es",numero2)
+if numero3>numero1:
+    print("El numero mayor es",numero3)
+if  numero3>numero2:
+    print("El numero mayor es",numero3)
+else:
+    print("Los numeros ingresados son iguales")
---
+++ RESIDUO DE DOS NUMEROS.py
@@ -0,0 +1,11 @@
+numero1=int(input("Ingrese el primer numero"))
+numero2=int(input("Ingrese el segundo numero"))
+if numero1>numero2:
+    print("El numero mayor es",numero1)
+if numero2>numero1:
+    print("El numero mayor es",numero2)
+else:
+    print("Los numeros ingresados son iguales")
+if numero1==numero2:
+     print("El cociente es",numero1/numero2)
+     print("El residuo es",numero1%numero2)
