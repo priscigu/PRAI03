389c38d951bd44c5f4b565c4bd4c600e713ec4e4
priscigu <110804906+priscigu@users.noreply.github.com>
2022-08-23 22:08:10 -0400

    Add files via upload

---
+++ HORAS EXTRAS(3).py
@@ -0,0 +1,14 @@
+preciohora= int(input("Ingrese el precio de hora"))
+cantidadhoras= int(input("Ingrese la cantidad de horas trabajadas"))
+if cantidahoras>40
+   preciohora<-50
+   pago<-cantidadhoras*preciohora:
+        print("Su total de pago es",pago)
+else:
+    preciohextras<-15
+    cantidad<-cantidahoras-40
+    pago<- 40*preciohora
+    pagohorasextras<-cantidad*preciohextras
+    salario<-pagohoraextras+pago:
+        print("Su salario es",salario)
+    
